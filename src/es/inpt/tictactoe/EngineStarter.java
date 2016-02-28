b891ed1bf20579175f44b18fcc57e2e6daad4d23
Iván Peña <ivan.penya@mimacom.com>
2016-02-28 16:30:21 +0100

    Removing unnecessary comments.

--- src/es/inpt/tictactoe/EngineStarter.java
+++ src/es/inpt/tictactoe/EngineStarter.java
@@ -9,8 +9,6 @@ import es.inpt.tictactoe.io.Parser;
 public class EngineStarter {
 	
 	public static void main(String[] args) throws IOException {
-//		Parser parser1 = new Parser(field, "tictactoebotSquare", 1);
-//		Parser parser2 = new Parser(field, "tictactoebotPosition", 2);
 		
 		if (args.length != 2) {
 			System.out.println("Incorrect number of parameters. You should specify two different names that matches with the file name of your bots.");
